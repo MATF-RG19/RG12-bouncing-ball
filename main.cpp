9365993068a694afce2a84657a0485e6ad7fe6af
UBUNTU <markokulis.markokulis@gmail.com>
2020-01-12 15:29:46 +0100

    Dodat highscore, health, reset...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <iostream>
 #include <cstdio>
 #include <cmath>
@@ -6,6 +7,7 @@
 #include <cstdlib>
 #include <vector>
 #include <queue>
+#include <string>
 
 #define TIMER_INTERVAL 20
 #define TIMER_ID 0
@@ -19,16 +21,18 @@ static void on_keyboard(unsigned char key, int x, int y);
 static void on_timer(int id);
 
 static void draw_axis(float len);
+static void text_log(float x, float z, const char *s);
 static void draw_ball();
 static void draw_floor();
 void obstacles_init();
 void quadricsInit1();
-void draw_obastacles();
+void draw_obstacles();
 void move_forward(double val1);
 void move_left(double val1);
 void move_right(double val1);
 void obstacle_renew();
 
+char buffer[30];
 
 double translation_animate = 0.3;
 double translation_rotate = 1;
@@ -36,10 +40,11 @@ double rotira_se_levo;
 double rotira_se_desno;
 double faktor_ubrzanja = 0;
 double faktor_skretanja = 0;
-double faktorRot = 0;
+double faktor_rot = 0; //
 
 int broj_prepreka = 100;
-
+int health = 3;
+int max_param = -1;
 int index = 0;
 int pozmax = broj_prepreka - 1;
 
@@ -50,6 +55,7 @@ int animation_ongoing = 0;
 int animation_parameter = 0;
 
 bool initovane_prepreke = false;
+bool blink_ball = false;
 
 GLUquadric *qobj;
 
@@ -89,7 +95,7 @@ int main(int argc, char **argv)
     quadricsInit1();
     
 
-    float light_position[] = {10,10, 10, 1};
+    float light_position[] = {10, 10, 10, 0};
     float light_ambient[] = {.3f, .3f, .3f, 1};
     float light_diffuse[] = {.7f, .7f, .7f, 1};
     float light_specular[] = {.7f, .7f, .7f, 1};
@@ -102,14 +108,8 @@ int main(int argc, char **argv)
 
     glClearColor(0.24, 0.53, 0.77, 1);
     glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, 1);
-    
 
 	obstacles_init();
- 	
- 	for(int i=0; i < broj_prepreka; i++)
-	{
-		cout<< prepreke[i].pozx<<" "<<prepreke[i].pozy<<" "<< prepreke[i].pozz <<endl;
-	}
  	
     glutMainLoop();
 
@@ -146,6 +146,17 @@ void quadricsInit1(){
     
 }
 
+static void text_log( float x, float z, const char *s)
+{
+    glDisable(GL_LIGHT0);
+
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+    glRasterPos3f(x, -6, z);
+    glutBitmapString(GLUT_BITMAP_HELVETICA_18, reinterpret_cast<const unsigned char *>( s ) ); 
+
+    glEnable(GL_LIGHT0);
+}
+
 void on_keyboard(unsigned char key, int x, int y) {
     switch(key) {
         case 'w':
@@ -158,13 +169,13 @@ void on_keyboard(unsigned char key, int x, int y) {
         		break;
         case 'a':
         case 'A':
-        		faktorRot += 0.3; 
+        		faktor_rot += 0.3; 
         		rotira_se_levo = 1;
         		rotira_se_desno = 0;    			
         		break;
         case 'd':
         case 'D':
-        		faktorRot -= 0.3;
+        		faktor_rot -= 0.3;
         		rotira_se_levo = 0;
         		rotira_se_desno = 1;
         		break;
@@ -181,8 +192,15 @@ void on_keyboard(unsigned char key, int x, int y) {
             animation_ongoing = 0;
             break;
         case 'r':
-        case 'R':
+        case 'R': //popravi ne radi lepo
             animation_parameter = 0;
+            faktor_ubrzanja = 0;
+            translation_rotate = 1;
+            health = 3;
+            index = 0;
+			pozmax = broj_prepreka - 1;	
+            obstacle_renew();
+            obstacles_init();
             glutPostRedisplay();
             break;
         /*
@@ -200,7 +218,7 @@ void on_timer(int id) {
 		if(animation_ongoing){
 	
 			animation_parameter++;
-			if(animation_parameter%1600 == 0)       //koliko cesto se ubrzava
+			if(animation_parameter%1000 == 0)       //koliko cesto se ubrzava
 			{		
 				faktor_ubrzanja += 0.05;         // ovde se igrica ubrzava
 				translation_rotate += 0.1;		//srazmerno povecati i brzinu rotacije
@@ -210,8 +228,11 @@ void on_timer(int id) {
 		
 		obstacle_renew();
 		
-		move_forward(translation_animate + faktor_ubrzanja);
+		/*text_log(20,20, "POENI");*/
 		
+		move_forward(translation_animate + faktor_ubrzanja);
+
+		//dodati W za pravo?
 		if(rotira_se_levo == 1 && rotira_se_desno == 0)
 			move_left(translation_rotate); //param_left
 		else
@@ -267,25 +288,28 @@ void move_right(double val1)
 void draw_ball(){
     glPushMatrix();
     
+    //printf("LOPTA");
+
+    GLfloat diffuse1[] = { 0.24, 0.53, 0.77, 0};
     GLfloat ambient1[] = {0.3,0.3,0.3,1};
-    GLfloat diffuse1[] = { 0.24, 0.53, 0.77, 0}; //0.67,0.76,0.65,0
     GLfloat specular1[] = {0.6,0.6,0.6,1};
     GLfloat shininess1 = 80;
+    /*
+    if(blink_ball){
+        srand(time(NULL));
+        diffuse1[0] = rand()%11/10.0;
+        diffuse1[1] = rand()%11/10.0;
+        diffuse1[2] = rand()%11/10.0;
+    }*/
 
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient1);
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular1);
     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess1);
     
-    /*
-    glPushMatrix();
-     	glRotatef(0, 0, 1, 0);
-		glTranslatef(1.2, -6.2, 0); // 0.75 je min visina za x koord
-		glScalef(3, 3, 1.2);
-		glutSolidCube(1);
-    glPopMatrix();*/
-    glTranslatef(2.55, -8, 0); 
-    glutSolidSphere(0.3, 30, 30);
+    glTranslatef(2.55, -8, 0);
+    glRotatef((4* animation_parameter)%360, 0, 0, 1);
+    glutSolidSphere(0.3, 50, 50);
     
     
     //gluCylinder(qobj, 10, 1.0, 0.4, 20, 20);
@@ -307,7 +331,7 @@ void draw_floor(){
     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 
 	 glTranslatef(0,-100, 0);
-	 glRotatef(faktorRot, 0, 1, 0);
+	 glRotatef(faktor_rot, 0, 1, 0);
     glRotatef(-90, 1, 0, 0);
     gluCylinder(qobj, 2.4, 2.0, 400, 20, 20);
     glTranslatef(0,100, 0);
@@ -360,18 +384,32 @@ void draw_obstacle(int rot, double pomeraj, double visina){
 	glPopMatrix();
 }
 
-void draw_obastacles(){
-		//velicina vekt je A*B
-	for(int i=0; i < broj_prepreka; i++) //A
-	{	
-		if((prepreke[i].pozz >= 0 && prepreke[i].pozz <=20 
-		|| (prepreke[i].pozz >= 340 && prepreke[i].pozz <360))
-		 && (prepreke[i].pozy >= (-9.8 ) && prepreke[i].pozy <= (-5.2)))
-		{
-			animation_ongoing = 0;
-			cout<< "Kolizija"<<endl;
-		}
-			
+void draw_obstacles(){
+	
+	for(int i=0; i < broj_prepreka; i++)
+	{
+        if(max_param == -1){
+
+            if((prepreke[i].pozz >= 0 && prepreke[i].pozz <=20 
+            || (prepreke[i].pozz >= 340 && prepreke[i].pozz <360))
+            && (prepreke[i].pozy >= (-9.6) && prepreke[i].pozy <= (-5.2)))
+            {
+                max_param = animation_parameter;
+                health--;
+                
+                if(health < 1) animation_ongoing = 0;
+
+                cout<< animation_parameter <<endl;
+            }
+        }
+        else if(max_param + 50 <= animation_parameter){
+            max_param = -1;
+            blink_ball = false; //ovaj deo
+        }
+        else{                   //ovaj deo
+            blink_ball = true; 
+        }
+
 		draw_obstacle(prepreke[i].pozz, prepreke[i].pozy, prepreke[i].pozx);
 	}
 }
@@ -404,6 +442,13 @@ void on_display() {
               0, 200, 0,
               0, 1, 0);
 
+    sprintf(buffer, "Score : %d ", animation_parameter);
+    text_log(6.2, 2.5, buffer);
+    
+    sprintf(buffer, "Health : %d ", health);
+    text_log(6, 2.5, buffer);
+
+
     //draw_axis(5);
     glPushMatrix();
 
@@ -415,7 +460,7 @@ void on_display() {
     	draw_obstacle(7, -5, 1);
     glPopMatrix();*/
    	
-    draw_obastacles();
+    draw_obstacles();
 
     glPushMatrix();
     	//napisi rotaciju lopte
